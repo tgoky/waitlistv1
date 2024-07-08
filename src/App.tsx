d6d20e93b2c830604407a5e645d23eeb047ef1f3
tgoky <tgokyicloud6@gmail.com>
2024-07-08 08:37:23 +0100

    d

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ const App: React.FC = () => {
       <div className="content">
         <h1></h1>
         <p>Join our waitlist to stay updated!</p>
+        <div className="image-container">
+          <img src="/sa1.png" alt="Image 1" style={{width: '200px', height: '200px'}}/>
+          <img src="/sa2.png" alt="Image 2"style={{width: '200px', height: '200px'}} />
+          <img src="/sa3.png" alt="Image 2" style={{width: '200px', height: '200px'}}/>
+        </div>
       </div>
     </div>
   );
